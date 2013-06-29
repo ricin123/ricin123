f62f70523b7d06c462bbda433a38c5e87bd5669a
Hou Dingding <460471121@qq.com>
2013-06-29 17:32:40 +0800

    1
    test

---
+++ test.py
@@ -0,0 +1,16 @@
+#def printmax(a,b):
+#	if a>b:
+#		print (a,'is max')
+#	else:
+#		print (b,'is max')
+#
+#printmax(5,6)
+
+#x=7
+#y=5
+#printmax(x,y)
+
+def say(message,times = 1):
+	print(message * times)
+say('hello')
+say('he',4)
